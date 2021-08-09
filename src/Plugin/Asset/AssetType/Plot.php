3800861293a9fc1493cf497c8a3a0c0abb78836c
Barnaby Norman <barnabynorman@users.noreply.github.com>
2021-08-09 11:27:15 -0400

    Add Plot asset type.

---
+++ src/Plugin/Asset/AssetType/Plot.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_plot\Plugin\Asset\AssetType;
+
+use Drupal\farm_entity\Plugin\Asset\AssetType\FarmAssetType;
+
+/**
+ * Provides the plot asset type.
+ *
+ * @AssetType(
+ *   id = "plot",
+ *   label = @Translation("Plot"),
+ * )
+ */
+class Plot extends FarmAssetType {
+
+}
