0226d79ce6e5aa37958e920d770676f77edc31a3
Michael Stenta <mike@mstenta.net>
2021-08-09 12:16:37 -0400

    Add Experiment Plan type.

---
+++ src/Plugin/Plan/PlanType/Experiment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_rothamsted\Plugin\Plan\PlanType;
+
+use Drupal\farm_entity\Plugin\Plan\PlanType\FarmPlanType;
+
+/**
+ * Provides the experiment plan type.
+ *
+ * @PlanType(
+ *   id = "rothamsted_experiment",
+ *   label = @Translation("Experiment"),
+ * )
+ */
+class Experiment extends FarmPlanType {
+
+}
