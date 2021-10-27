2ef5b1fc5663c38147bb0d509762c36141e4af36
Barnaby Norman <barnabynorman@users.noreply.github.com>
2021-10-27 09:54:34 +0100

    Add field in plan to hold json and extract factors from upload into field

--- src/Form/UploadExperimentForm.php
+++ src/Form/UploadExperimentForm.php
@@ -128,12 +128,16 @@ class UploadExperimentForm extends FormBase {
     $data = file_get_contents($file->getFileUri());
     $json = Json::decode($data);
 
+    // Extract factors json
+    $factors = $json['factors'];
+
     // create and save new plan based on crs name
     $plan = Plan::create(
           [
             'type' => 'rothamsted_experiment',
             'name' => $json['name'],
             'status' => 'active',
+            'factors' => Json::encode($factors),
           ]
       );
     $plan->save();
