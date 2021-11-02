24a3d121651366d1f0cb207d1113e7d9dc717b04
Paul Weidner <paul.weidner@gmail.com>
2021-11-02 12:39:48 -0700

    Use correct geojson path in plot_layer js behavior.

--- js/farmOS.map.behaviors.farm_rothamsted_experiment_plot_layer.js
+++ js/farmOS.map.behaviors.farm_rothamsted_experiment_plot_layer.js
@@ -1,7 +1,11 @@
 (function () {
-  farmOS.map.behaviours.farm_rothamsted_experiment_plot_layer = {
+  farmOS.map.behaviors.farm_rothamsted_experiment_plot_layer = {
     attach: function (instance) {
-      var url = new URL('/nfa-assets/geojson/' + instance.farmMapSettings.plan, window.location.origin + drupalSettings.path.baseUrl)
+      const planId = instance.farmMapSettings.farm_rothamsted_experiment_plot_layer.plan;
+      var url = new URL(
+        `/plan/${planId}/plots/geojson`,
+        window.location.origin + drupalSettings.path.baseUrl
+      )
       var newLayer = instance.addLayer('geojson', {
         title: Drupal.t('Plots'),
         url,
