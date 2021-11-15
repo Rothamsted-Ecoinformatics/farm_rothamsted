388cd2f4046c91f33f946ac55721d97679bbdceb
Barnaby Norman <barnabynorman@users.noreply.github.com>
2021-11-15 15:53:56 +0000

    Add unit field and submit method

--- src/Plugin/QuickForm/QuickHarvest.php
+++ src/Plugin/QuickForm/QuickHarvest.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace Drupal\farm_rothamsted\Plugin\QuickForm;
-
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\farm_quick\QuickFormBase;
+use Drupal\farm_quick\Traits\QuickLogTrait;
 
 /**
  * Harvest quick form.
@@ -19,6 +20,8 @@ use Drupal\Core\Form\FormStateInterface;
  */
 class QuickHarvest extends QuickExperimentFormBase {
 
+  use QuickLogTrait;
+
   /**
    * {@inheritdoc}
    */
@@ -27,15 +30,61 @@ class QuickHarvest extends QuickExperimentFormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildForm($form, $form_state);
+  public function buildForm(array $form, FormStateInterface $form_state, string $id = NULL) {
 
-    // @todo Number of quantities.
+    // Harvest quantity.
+    $form['quantity'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Quantitly'),
+      '#min' => 0,
+      '#step' => 1,
+      '#required' => TRUE,
+    ];
+
+    // Harvest units.
+    $harvest_units = [
+      '',
+      'g',
+      'kg',
+      'Gt'
+    ];
+
+    // @todo Each harvest - units from hard coded list.
+    $form['units'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Units'),
+      '#options' => array_combine($harvest_units, $harvest_units),
+    ];
 
     // @todo Each quantity - measure, value, units, label.
     // @todo AJAX for each quantity.
 
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    // Draft an harvest log from the user-submitted data.
+    $quantity = $form_state->getValue('quantity');
+    $units = $form_state->getValue('units');
+
+    $log = [
+      'name' => $this->t('Harvested @quantity @units', ['@quantity' => $quantity, '@units' => $units]),
+      'type' => 'harvest',
+      'quantity' => [
+        [
+          'measure' => 'unit',
+          'value' => $quantity,
+          'unit' => $units,
+        ],
+      ],
+    ];
+
+    // Create the log.
+    $this->createLog($log);
   }
 
 }
