21b34c71176cfc009c4dcccb1a6b0961ae120199
Paul Weidner <paul.weidner@gmail.com>
2021-11-15 12:37:33 -0800

    Use exposed filters when requesting plan_plot geojson.

--- js/farmOS.map.behaviors.farm_rothamsted_experiment_plot_layer.js
+++ js/farmOS.map.behaviors.farm_rothamsted_experiment_plot_layer.js
@@ -1,11 +1,26 @@
 (function () {
   farmOS.map.behaviors.farm_rothamsted_experiment_plot_layer = {
     attach: function (instance) {
-      const planId = instance.farmMapSettings.farm_rothamsted_experiment_plot_layer.plan;
+      const layerOptions = instance.farmMapSettings.farm_rothamsted_experiment_plot_layer;
       var url = new URL(
-        `/plan/${planId}/plots/geojson`,
+        `/plan/${layerOptions.plan}/plots/geojson`,
         window.location.origin + drupalSettings.path.baseUrl
       )
+
+      // Include provided filters.
+      const filters = layerOptions.filters ?? {};
+      Object.entries(filters).forEach( ([key, value]) => {
+        if (Array.isArray(value)) {
+          for (let i = 0; i < value.length; i++) {
+            url.searchParams.append(key + '[]', value[i]);
+          }
+        }
+        else {
+          url.searchParams.append(key, value);
+        }
+      });
+
+      // Create the layer.
       var newLayer = instance.addLayer('geojson', {
         title: Drupal.t('Plots'),
         url,
