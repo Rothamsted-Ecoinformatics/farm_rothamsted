c156bf8f75a6a29e7290eb47cb71091fac05a2f1
Paul Weidner <paul.weidner@gmail.com>
2021-12-06 13:43:10 -0800

    Use the plan name instead of id for the land asset.

--- src/Form/UploadExperimentForm.php
+++ src/Form/UploadExperimentForm.php
@@ -159,7 +159,7 @@ class UploadExperimentForm extends FormBase {
     $experiment_land = Asset::create([
       'type' => 'land',
       'land_type' => 'other',
-      'name' => $this->t('Experiment @plan_id Surrounds', ['@plan_id' => $plan->id()]),
+      'name' => $this->t('@plan_name Experiment Surrounds', ['@plan_name' => $plan->label()]),
       'status' => 'active',
       'is_fixed' => TRUE,
       'is_location' => TRUE,
