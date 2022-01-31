0c3b24d41eddc88a998911a8bd7e91dd333d36fd
Barnaby Norman <barnabynorman@users.noreply.github.com>
2022-01-31 11:09:55 +0000

    Remove class that forced radio units inline as breaking title layout

--- src/Plugin/QuickForm/QuickExperimentFormBase.php
+++ src/Plugin/QuickForm/QuickExperimentFormBase.php
@@ -125,8 +125,10 @@ abstract class QuickExperimentFormBase extends QuickFormBase {
   protected function buildQuantityUnitsElement(array $config) {
     $element = [];
 
+    $titleLabel = '';
+
     // Wrapper to keep everything inline with optional description field at end.
-    $element['#prefix'] = '<div class="form-items-inline container-inline">';
+    $element['#prefix'] = '<div class="form-items-inline">';
 
     $suffix = '</div>';
     if (isset($config['#description'])) {
