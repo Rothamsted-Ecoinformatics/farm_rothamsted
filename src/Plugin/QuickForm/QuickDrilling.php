9f8569f8654721c477ecb77838ca014c0974b8af
Paul Weidner <paul.weidner@gmail.com>
2022-03-04 11:22:48 -0800

    Implement changes for the drilling tab fields.

--- src/Plugin/QuickForm/QuickDrilling.php
+++ src/Plugin/QuickForm/QuickDrilling.php
@@ -45,9 +45,6 @@ class QuickDrilling extends QuickExperimentFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildForm($form, $form_state);
 
-    // Add to the operation tab.
-    $operation = $form['operation'];
-
     // Drilling tab.
     $drilling = [
       '#type' => 'details',
@@ -90,7 +87,7 @@ class QuickDrilling extends QuickExperimentFormBase {
     $drilling['crop']['crop_variety'] = [
       '#type' => 'select',
       '#title' => $this->t('Variety(s)'),
-      '#description' => $this->t('The variety(s) being planted.'),
+      '#description' => $this->t('The variety(s) being planted. To select more than one option on a desktop PC hold down the CTRL button on and select multiple.'),
       '#options' => $crop_variety_options,
       '#multiple' => TRUE,
       '#required' => TRUE,
@@ -101,14 +98,14 @@ class QuickDrilling extends QuickExperimentFormBase {
 
     // Target plant population units options.
     $target_plant_population_units_options = [
-      'plants/ m2' => 'plants/ m2',
+      'plants/m2' => 'plants/m2',
       '%' => '%',
     ];
 
     // Seed rate.
     $seed_rate_units_options = [
-      'seeds/ m2' => 'seeds/ m2',
-      'units/ha' => 'units/ha',
+      'seeds/m2' => 'seeds/m2',
+      'plants/ha' => 'plants/ha',
     ];
     $seed_rate = [
       'title' => $this->t('Seed rate'),
@@ -120,20 +117,15 @@ class QuickDrilling extends QuickExperimentFormBase {
     $drilling['seed_rate'] = $this->buildQuantityField($seed_rate);
 
     // Drilling rate.
+    $drilling_rate_units_options = [
+      'kg/ha' => 'kg/ha',
+      'units/ha' => 'units/ha',
+    ];
     $drilling['drilling_rate'] = $this->buildQuantityField([
       'title' => $this->t('Drilling rate'),
       'description' => $this->t('The volume of seed drilled per unit area. This information must be provided as it is essential information for scientists wanting to analyse the crop data.'),
       'measure' => ['#value' => 'rate'],
-      'units' => ['#value' => 'kg/ha'],
-      'required' => TRUE,
-    ]);
-
-    // Seed weight.
-    $drilling['seed_weight'] = $this->buildQuantityField([
-      'title' => $this->t('Seed weight'),
-      'description' => $this->t('The total amount of seed needed for a given area.'),
-      'measure' => ['#value' => 'weight'],
-      'units' => ['#value' => 'kg'],
+      'units' => ['#options' => $drilling_rate_units_options],
       'required' => TRUE,
     ]);
 
@@ -147,31 +139,19 @@ class QuickDrilling extends QuickExperimentFormBase {
       '#multiple' => TRUE,
     ];
 
-    // Product.
-    $product_options = $this->getChildTermOptionsByName('material_type', 'Starter Fertiliser');
-    $drilling['product'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Product'),
-      '#description' => $this->t('The product used as a starter fertiliser.'),
-      '#options' => $product_options,
-      '#required' => FALSE,
+    // Seed labels.
+    $drilling['seed_labels'] = [
+      '#type' => 'managed_file',
+      '#title' => $this->t('Seed labels'),
+      '#description' => $this->t('Photograph(s) of the seed label taken prior to drilling or confirm the right seed batch and variety was used.'),
+      '#upload_location' => $this->getFileUploadLocation('log', $this->logType, 'image'),
+      '#upload_validators' => [
+        'file_validate_extensions' => self::$validImageExtensions,
+      ],
+      '#multiple' => TRUE,
+      '#extended' => TRUE,
+      '#required' => TRUE,
     ];
-
-    // Product application rate.
-    $product_application_rate_units_options = [
-      'l/ha' => 'l/ha',
-      'kg/ha' => 'kg/ha',
-      'ml/ha' => 'ml/ha',
-      'g/ha' => 'g/ha',
-      't/ha' => 't/ha',
-    ];
-    $product_application_rate = [
-      'title' => $this->t('Product application rate'),
-      'description' => $this->t('The volume of product per unit that needs to be applied in order to achieve the desired nutrient rate(s).'),
-      'measure' => ['#value' => 'rate'],
-      'units' => ['#options' => $product_application_rate_units_options],
-    ];
-    $drilling['product_application_rate'] = $this->buildQuantityField($product_application_rate);
 
     // Add the drilling tab and fields to the form.
     $form['drilling'] = $drilling;
@@ -203,13 +183,13 @@ class QuickDrilling extends QuickExperimentFormBase {
 
     // Establishment average.
     $establishment_average_units_options = [
-      'plants/ m2' => 'plants/ m2',
+      'plants/m2' => 'plants/m2',
       '%' => '%',
     ];
     $establishment_average = [
       'title' => $this->t('Establishment average'),
       'description' => $this->t('The estimated plant establishment after drilling as a percentage. This is usually based on previous field records over the last 2- 5 years.'),
-      'measure' => ['#value' => 'volume'],
+      'measure' => ['#value' => 'ratio'],
       'units' => ['#options' => $establishment_average_units_options],
     ];
     $additional['establishment_average'] = $this->buildQuantityField($establishment_average);
@@ -237,44 +217,6 @@ class QuickDrilling extends QuickExperimentFormBase {
       $form['products'][$field_name] = $form['setup'][$field_name];
       unset($form['setup'][$field_name]);
     }
-
-    // Seed lot wrapper.
-    $operation['seed_lots'] = $this->buildInlineWrapper();
-
-    // Farm seed lot.
-    $operation['seed_lots']['farm_seed_lot'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Farm seed lot'),
-      '#description' => $this->t('The seed lot number assigned by the farm.'),
-      '#required' => TRUE,
-      '#size' => 30,
-    ];
-
-    // Supplier seed lot.
-    $operation['seed_lots']['supplier_seed_lot'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Supplier seed lot'),
-      '#description' => $this->t('The seed lot number provided by the supplier, if available.'),
-      '#required' => FALSE,
-      '#size' => 30,
-    ];
-
-    // Seed labels.
-    $operation['seed_labels'] = [
-      '#type' => 'managed_file',
-      '#title' => $this->t('Seed labels'),
-      '#description' => $this->t('Photograph(s) of the seed label taken prior to drilling or confirm the right seed batch and variety was used.'),
-      '#upload_location' => $this->getFileUploadLocation('log', $this->logType, 'image'),
-      '#upload_validators' => [
-        'file_validate_extensions' => self::$validImageExtensions,
-      ],
-      '#multiple' => TRUE,
-      '#extended' => TRUE,
-      '#required' => TRUE,
-    ];
-
-    // Add the operation tab and fields to the form.
-    $form['operation'] = $operation;
 
     return $form;
   }
@@ -304,15 +246,13 @@ class QuickDrilling extends QuickExperimentFormBase {
   protected function getQuantities(array $field_keys, FormStateInterface $form_state): array {
     array_push(
       $field_keys,
-      'target_plant_population',
-      'establishment_average',
-      'germination_rate',
-      'thousand_grain_weight',
       'seed_rate',
       'drilling_rate',
-      'seed_weight',
+      'thousand_grain_weight',
+      'germination_rate',
+      'target_plant_population',
+      'establishment_average',
       'drilling_depth',
-      'product_application_rate',
     );
     return parent::getQuantities($field_keys, $form_state);
   }
