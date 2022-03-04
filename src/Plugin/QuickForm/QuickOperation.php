496d70b5c100a082744ea6522d9b6314768e2cfa
Paul Weidner <paul.weidner@gmail.com>
2022-03-03 17:28:39 -0800

    Update operations form id to field_operations.

--- src/Plugin/QuickForm/QuickOperation.php
+++ src/Plugin/QuickForm/QuickOperation.php
@@ -7,12 +7,8 @@ use Drupal\Core\Form\FormStateInterface;
 /**
  * Operations quick form.
  *
- * @todo This was previously the cultivation quick form so we maintain that ID.
- *
- * @see https://github.com/Rothamsted-Ecoinformatics/farm_rothamsted/pull/6#issuecomment-903958799
- *
  * @QuickForm(
- *   id = "farm_rothamsted_cultivation_quick_form",
+ *   id = "field_operations",
  *   label = @Translation("Field operations"),
  *   description = @Translation("Create operation records."),
  *   helpText = @Translation("Use this form to record operation records."),
