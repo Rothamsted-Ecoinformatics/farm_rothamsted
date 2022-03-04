01ec22d7b1e11d043b8e60491191ecbcde6b36ef
Paul Weidner <paul.weidner@gmail.com>
2022-03-04 11:12:40 -0800

    Add Additional information tab.

--- src/Plugin/QuickForm/QuickDrilling.php
+++ src/Plugin/QuickForm/QuickDrilling.php
@@ -56,6 +56,14 @@ class QuickDrilling extends QuickExperimentFormBase {
       '#weight' => 0,
     ];
 
+    // Additional information tab.
+    $additional = [
+      '#type' => 'details',
+      '#title' => $this->t('Additional information'),
+      '#group' => 'tabs',
+      '#weight' => 1,
+    ];
+
     // Crop and variety wrapper.
     $drilling['crop'] = $this->buildInlineWrapper();
 
@@ -97,44 +105,6 @@ class QuickDrilling extends QuickExperimentFormBase {
       '%' => '%',
     ];
 
-    // Target plant population.
-    $target_plant_population = [
-      'title' => $this->t('Target plant population'),
-      'description' => $this->t('The target population for plant establishment after drilling.'),
-      'measure' => ['#value' => 'ratio'],
-      'units' => ['#options' => $target_plant_population_units_options],
-    ];
-    $drilling['target_plant_population'] = $this->buildQuantityField($target_plant_population);
-
-    // Establishment average.
-    $establishment_average_units_options = [
-      'plants/ m2' => 'plants/ m2',
-      '%' => '%',
-    ];
-    $establishment_average = [
-      'title' => $this->t('Establishment average'),
-      'description' => $this->t('The estimated plant establishment after drilling as a percentage. This is usually based on previous field records over the last 2- 5 years.'),
-      'measure' => ['#value' => 'volume'],
-      'units' => ['#options' => $establishment_average_units_options],
-    ];
-    $drilling['establishment_average'] = $this->buildQuantityField($establishment_average);
-
-    // Germination rate.
-    $drilling['germination_rate'] = $this->buildQuantityField([
-      'title' => $this->t('Germination rate'),
-      'description' => $this->t('The germination rate of the seed batch, measured by placing 50 to 100 seeds in a sealed tupperware box lined with wet kitchen roll and counting the number of seeds germinated after 10 - 14 days.'),
-      'measure' => ['#value' => 'ratio'],
-      'units' => ['#value' => '%'],
-    ]);
-
-    // Thousand grain weight.
-    $drilling['thousand_grain_weight'] = $this->buildQuantityField([
-      'title' => $this->t('Thousand grain weight (TGW)'),
-      'description' => $this->t('The average weight of 1,000 grains.'),
-      'measure' => ['#value' => 'weight'],
-      'units' => ['#value' => 'g'],
-    ]);
-
     // Seed rate.
     $seed_rate_units_options = [
       'seeds/ m2' => 'seeds/ m2',
@@ -165,14 +135,6 @@ class QuickDrilling extends QuickExperimentFormBase {
       'measure' => ['#value' => 'weight'],
       'units' => ['#value' => 'kg'],
       'required' => TRUE,
-    ]);
-
-    // Drilling depth.
-    $drilling['drilling_depth'] = $this->buildQuantityField([
-      'title' => $this->t('Drilling depth'),
-      'description' => $this->t('The estimate of the depth at which the seed was drilled. It is important to take this info account when reviewing establishment avarages.'),
-      'measure' => ['#value' => 'length'],
-      'units' => ['#value' => 'cm'],
     ]);
 
     // Seed dressings.
@@ -211,25 +173,64 @@ class QuickDrilling extends QuickExperimentFormBase {
     ];
     $drilling['product_application_rate'] = $this->buildQuantityField($product_application_rate);
 
-    // Seed lineage options.
-    $seed_lineage_options = [
-      'red' => 'red',
-      'blue' => 'blue',
-      'green' => 'green',
-    ];
-
-    // Product.
-    $drilling['seed_lineage'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Seed lineage'),
-      '#description' => $this->t('The plant asset(s) which the seed came from.'),
-      '#options' => $seed_lineage_options,
-      '#multiple' => TRUE,
-      '#required' => FALSE,
-    ];
-
     // Add the drilling tab and fields to the form.
     $form['drilling'] = $drilling;
+
+    // Thousand grain weight.
+    $additional['thousand_grain_weight'] = $this->buildQuantityField([
+      'title' => $this->t('Thousand grain weight (TGW)'),
+      'description' => $this->t('The average weight of 1,000 grains.'),
+      'measure' => ['#value' => 'weight'],
+      'units' => ['#value' => 'g'],
+    ]);
+
+    // Germination rate.
+    $additional['germination_rate'] = $this->buildQuantityField([
+      'title' => $this->t('Seed Germination Test Result(s)'),
+      'description' => $this->t('The germination rate of the seed batch, measured by placing 50 to 100 seeds in a sealed tupperware box lined with wet kitchen roll and counting the number of seeds germinated after 10 - 14 days.'),
+      'measure' => ['#value' => 'ratio'],
+      'units' => ['#value' => '%'],
+    ]);
+
+    // Target plant population.
+    $target_plant_population = [
+      'title' => $this->t('Target plant population'),
+      'description' => $this->t('The target population for plant establishment after drilling.'),
+      'measure' => ['#value' => 'ratio'],
+      'units' => ['#options' => $target_plant_population_units_options],
+    ];
+    $additional['target_plant_population'] = $this->buildQuantityField($target_plant_population);
+
+    // Establishment average.
+    $establishment_average_units_options = [
+      'plants/ m2' => 'plants/ m2',
+      '%' => '%',
+    ];
+    $establishment_average = [
+      'title' => $this->t('Establishment average'),
+      'description' => $this->t('The estimated plant establishment after drilling as a percentage. This is usually based on previous field records over the last 2- 5 years.'),
+      'measure' => ['#value' => 'volume'],
+      'units' => ['#options' => $establishment_average_units_options],
+    ];
+    $additional['establishment_average'] = $this->buildQuantityField($establishment_average);
+
+    // Drilling depth.
+    $additional['drilling_depth'] = $this->buildQuantityField([
+      'title' => $this->t('Drilling depth'),
+      'description' => $this->t('The estimate of the depth at which the seed was drilled. It is important to take this info account when reviewing establishment avarages.'),
+      'measure' => ['#value' => 'length'],
+      'units' => ['#value' => 'cm'],
+    ]);
+
+    // Seed lineage.
+    $additional['seed_lineage'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Seed lineage'),
+      '#description' => $this->t('The plant asset(s) which the seed came from.'),
+    ];
+
+    // Add the additional information tab and fields to the form.
+    $form['additional'] = $additional;
 
     // Move recommendation fields to products applied tab.
     foreach (['recommendation_number', 'recommendation_files'] as $field_name) {
