178c81e97d765a982912a41a905ac98d45c34983
Paul Weidner <paul.weidner@gmail.com>
2022-04-04 19:30:53 -0700

    Change the fertiliser product_labels to be required as originally specified.

--- modules/farm_rothamsted_quick/src/Plugin/QuickForm/QuickFertiliser.php
+++ modules/farm_rothamsted_quick/src/Plugin/QuickForm/QuickFertiliser.php
@@ -67,9 +67,7 @@ class QuickFertiliser extends QuickExperimentFormBase {
     ];
 
     // Make the product labels required.
-    // @todo Make this field required.
-    // If a file is uploaded after the products are filled in then it breaks.
-    $fertiliser['product_labels']['#required'] = FALSE;
+    $fertiliser['product_labels']['#required'] = TRUE;
 
     // Spray application rate units.
     $application_rate_units_options = $this->getChildTermOptionsByName('unit', 'Volume per unit area');
