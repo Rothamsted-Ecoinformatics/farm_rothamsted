c2e80cd298b532b77c0b6e74b77a0149f0a70114
Paul Weidner <paul.weidner@gmail.com>
2022-09-19 14:46:23 -0700

    Add experiment link fields for external documents. #284

--- modules/farm_rothamsted_experiment/farm_rothamsted_experiment.post_update.php
+++ modules/farm_rothamsted_experiment/farm_rothamsted_experiment.post_update.php
@@ -5,6 +5,7 @@
  * Update hooks for farm_rothamsted_experiment.module.
  */
 
+use Drupal\entity\BundleFieldDefinition;
 use Symfony\Component\Yaml\Yaml;
 
 /**
@@ -15,4 +16,50 @@ function farm_rothamsted_experiment_post_update_create_experiment_boundary_land_
   $config_path = \Drupal::service('extension.list.module')->getPath('farm_rothamsted_experiment') . "/config/install/farm_land.land_type.$land_type.yml";
   $data = Yaml::parseFile($config_path);
   \Drupal::configFactory()->getEditable("farm_land.land_type.$land_type")->setData($data)->save(TRUE);
+}
+
+/**
+ * Create experiment_boundary land type.
+ */
+function farm_rothamsted_experiment_post_update_create_experiment_link_fields(&$sandbox = NULL) {
+
+  // Install the link module.
+  if (\Drupal::moduleHandler()->moduleExists('link')) {
+    \Drupal::service('module_installer')->install(['link']);
+  }
+
+  // Experiment file link fields.
+  $fields['experiment_plan_link'] = BundleFieldDefinition::create('link')
+    ->setLabel(t('Experiment plan'))
+    ->setRequired(FALSE)
+    ->setRevisionable(TRUE)
+    ->setSettings([
+      'title' => DRUPAL_DISABLED,
+      // @see LinkItemInterface::LINK_EXTERNAL.
+      'link_type' => 0x10,
+    ])
+    ->setDisplayConfigurable('form', TRUE)
+    ->setDisplayConfigurable('view', TRUE);
+  $fields['experiment_file_link'] = BundleFieldDefinition::create('link')
+    ->setLabel(t('Experiment file'))
+    ->setRequired(FALSE)
+    ->setRevisionable(TRUE)
+    ->setSettings([
+      'title' => DRUPAL_DISABLED,
+      // @see LinkItemInterface::LINK_EXTERNAL.
+      'link_type' => 0x10,
+    ])
+    ->setDisplayConfigurable('form', TRUE)
+    ->setDisplayConfigurable('view', TRUE);
+
+  // Install each field definition.
+  foreach ($fields as $field_name => $field_definition) {
+    \Drupal::entityDefinitionUpdateManager()->installFieldStorageDefinition(
+      $field_name,
+      'plan',
+      'farm_rothamsted_experiment',
+      $field_definition,
+    );
+  }
+
 }
