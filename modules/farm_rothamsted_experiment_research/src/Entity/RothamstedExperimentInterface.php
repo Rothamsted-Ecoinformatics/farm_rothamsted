6febb045f30c294e7e60e0bb8ccf9e4c50013082
Paul Weidner <paul.weidner@gmail.com>
2023-02-21 14:25:22 -0800

    Add experiment entity #356

---
+++ modules/farm_rothamsted_experiment_research/src/Entity/RothamstedExperimentInterface.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\farm_rothamsted_experiment_research\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\Core\Entity\RevisionLogInterface;
+use Drupal\user\EntityOwnerInterface;
+
+/**
+ * Provides an interface for research experiment entities.
+ */
+interface RothamstedExperimentInterface extends ContentEntityInterface, EntityChangedInterface, RevisionLogInterface, EntityOwnerInterface {
+
+  /**
+   * Gets the experiment name.
+   *
+   * @return string
+   *   The experiment name.
+   */
+  public function getName();
+
+  /**
+   * Sets the experiment name.
+   *
+   * @param string $name
+   *   The experiment name.
+   *
+   * @return \Drupal\farm_rothamsted_experiment_research\Entity\RothamstedExperimentInterface
+   *   The experiment entity.
+   */
+  public function setName($name);
+
+  /**
+   * Gets the experiment creation timestamp.
+   *
+   * @return int
+   *   Creation timestamp of the experiment.
+   */
+  public function getCreatedTime();
+
+  /**
+   * Sets the experiment creation timestamp.
+   *
+   * @param int $timestamp
+   *   Creation timestamp of the experiment.
+   *
+   * @return \Drupal\farm_rothamsted_experiment_research\Entity\RothamstedExperimentInterface
+   *   The experiment entity.
+   */
+  public function setCreatedTime($timestamp);
+
+}
