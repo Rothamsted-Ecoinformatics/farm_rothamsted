e7f69f4d7560cb99edff7ceac59860c539f06cba
Paul Weidner <paul.weidner@gmail.com>
2023-02-21 14:16:36 -0800

    Add farm_rothamsted_researcher module #354

---
+++ modules/farm_rothamsted_researcher/src/Entity/RothamstedResearcherInterface.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\farm_rothamsted_researcher\Entity;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+use Drupal\Core\Entity\RevisionLogInterface;
+use Drupal\user\EntityOwnerInterface;
+
+/**
+ * Provides an interface for researcher entities.
+ */
+interface RothamstedResearcherInterface extends ContentEntityInterface, EntityChangedInterface, RevisionLogInterface, EntityOwnerInterface {
+
+  /**
+   * Gets the reseracher name.
+   *
+   * @return string
+   *   The researcher name.
+   */
+  public function getName();
+
+  /**
+   * Sets the researcher name.
+   *
+   * @param string $name
+   *   The researcher name.
+   *
+   * @return \Drupal\farm_rothamsted_researcher\Entity\RothamstedResearcherInterface
+   *   The researcher entity.
+   */
+  public function setName($name);
+
+  /**
+   * Gets the researcher creation timestamp.
+   *
+   * @return int
+   *   Creation timestamp of the researcher.
+   */
+  public function getCreatedTime();
+
+  /**
+   * Sets the researcher creation timestamp.
+   *
+   * @param int $timestamp
+   *   Creation timestamp of the researcher.
+   *
+   * @return \Drupal\farm_rothamsted_researcher\Entity\RothamstedResearcherInterface
+   *   The researcher entity.
+   */
+  public function setCreatedTime($timestamp);
+
+}
---
+++ modules/farm_rothamsted_researcher/src/RothamstedResearcherListBuilder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\farm_rothamsted_researcher;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityListBuilder;
+
+/**
+ * List builder for reserachers.
+ */
+class RothamstedResearcherListBuilder extends EntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header['name'] = $this->t('Name');
+    $header['role'] = $this->t('Role');
+    $header['organization'] = $this->t('Organisation');
+    $header['department'] = $this->t('Department');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    /** @var \Drupal\asset\Entity\AssetInterface $entity */
+    $row['name'] = $entity->toLink($entity->label(), 'canonical')->toString();
+    $row['role'] = $entity->get('role')->value;
+    $row['organization'] = $entity->get('organization')->value;
+    $row['department'] = $entity->get('department')->value;
+    return $row + parent::buildRow($entity);
+  }
+
+}
