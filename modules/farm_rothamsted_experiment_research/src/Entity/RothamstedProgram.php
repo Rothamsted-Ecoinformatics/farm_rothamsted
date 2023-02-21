dd4f6e7d1dd900135803535254c4b41601ced955
Paul Weidner <paul.weidner@gmail.com>
2023-02-21 14:25:05 -0800

    Add program entity #355

---
+++ modules/farm_rothamsted_experiment_research/src/Entity/RothamstedProgram.php
@@ -0,0 +1,313 @@
+<?php
+
+namespace Drupal\farm_rothamsted_experiment_research\Entity;
+
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\RevisionableContentEntityBase;
+use Drupal\Core\Entity\RevisionLogEntityTrait;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\user\EntityOwnerTrait;
+use Drupal\user\UserInterface;
+
+/**
+ * Defines the research program entity class.
+ *
+ * @ContentEntityType(
+ *   id = "rothamsted_program",
+ *   label = @Translation("Research Program"),
+ *   label_collection = @Translation("Research Programs"),
+ *   label_singular = @Translation("research program"),
+ *   label_plural = @Translation("research programs"),
+ *   handlers = {
+ *     "access" = "\Drupal\entity\UncacheableEntityAccessControlHandler",
+ *     "list_builder" = "Drupal\farm_rothamsted_experiment_research\RothamstedEntityListBuilder",
+ *     "permission_provider" = "\Drupal\entity\UncacheableEntityPermissionProvider",
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "form" = {
+ *       "add" = "Drupal\Core\Entity\ContentEntityForm",
+ *       "edit" = "Drupal\Core\Entity\ContentEntityForm",
+ *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm",
+ *     },
+ *     "route_provider" = {
+ *       "default" = "Drupal\entity\Routing\AdminHtmlRouteProvider",
+ *       "revision" = "\Drupal\entity\Routing\RevisionRouteProvider",
+ *     },
+ *     "local_task_provider" = {
+ *       "default" = "\Drupal\farm_rothamsted_experiment_research\Menu\DefaultSecondaryTaskProvider",
+ *     },
+ *   },
+ *   base_table = "rothamsted_program",
+ *   data_table = "rothamsted_program_data",
+ *   revision_table = "rothamsted_program_revision",
+ *   translatable = TRUE,
+ *   revisionable = TRUE,
+ *   show_revision_ui = TRUE,
+ *   admin_permission = "administer resarch programs",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "uuid" = "uuid",
+ *     "revision" = "revision_id",
+ *     "label" = "name",
+ *     "owner" = "uid",
+ *     "langcode" = "langcode",
+ *   },
+ *   revision_metadata_keys = {
+ *     "revision_user" = "revision_user",
+ *     "revision_created" = "revision_created",
+ *     "revision_log_message" = "revision_log_message",
+ *   },
+ *   links = {
+ *     "canonical" = "/rothamsted/program/{rothamsted_program}",
+ *     "collection" = "/rothamsted/program",
+ *     "add-form" = "/rothamsted/program/add",
+ *     "edit-form" = "/rothamsted/program/{rothamsted_program}/edit",
+ *     "delete-form" = "/rothamsted/program/{rothamsted_program}/delete",
+ *     "version-history" = "/rothamsted/program/{rothamsted_program}/revisions",
+ *     "revision" = "/rothamsted/program/{rothamsted_program}/revisions/{rothamsted_program_revision}/view",
+ *     "revision-revert-form" = "/rothamsted/program/{rothamsted_program}/revisions/{rothamsted_program_revision}/revert",
+ *   }
+ * )
+ */
+class RothamstedProgram extends RevisionableContentEntityBase implements RothamstedProgramInterface {
+
+  use EntityChangedTrait;
+  use EntityOwnerTrait;
+  use RevisionLogEntityTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function label() {
+    return $this->getName();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getName() {
+    return $this->get('name')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setName($name) {
+    $this->set('name', $name);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCreatedTime() {
+    return $this->get('created')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setCreatedTime($timestamp) {
+    $this->set('created', $timestamp);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwner() {
+    return $this->get('uid')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwnerId() {
+    return $this->get('uid')->target_id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwnerId($uid) {
+    $this->set('uid', $uid);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwner(UserInterface $account) {
+    $this->set('uid', $account->id());
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getCurrentUserId() {
+    return [\Drupal::currentUser()->id()];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+    $fields += static::ownerBaseFieldDefinitions($entity_type);
+    $fields += static::revisionLogBaseFieldDefinitions($entity_type);
+
+    $fields['name'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Name'))
+      ->setDescription(t('The name of the research program.'))
+      ->setRevisionable(TRUE)
+      ->setRequired(TRUE)
+      ->setSetting('max_length', 255)
+      ->setSetting('text_processing', 0)
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'string',
+        'weight' => -5,
+      ])
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('form', TRUE);
+
+    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Author'))
+      ->setDescription(t('The user ID of author of the research program.'))
+      ->setRevisionable(TRUE)
+      ->setDefaultValueCallback(static::class . '::getCurrentUserId')
+      ->setSetting('target_type', 'user')
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'region' => 'hidden',
+      ])
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('view', [
+        'region' => 'hidden',
+      ]);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Authored on'))
+      ->setDescription(t('The time that the research program was created.'))
+      ->setRevisionable(TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'region' => 'hidden',
+      ])
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('view', [
+        'region' => 'hidden',
+      ]);
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setDescription(t('The time that the research program was last edited.'))
+      ->setRevisionable(TRUE);
+
+    $fields['project_code'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Project code'))
+      ->setDescription(t('The project code assigned to the Research Programme.'))
+      ->setRevisionable(TRUE)
+      ->setRequired(TRUE)
+      ->setCardinality(FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -10,
+        'settings' => [
+          'size' => 25,
+        ],
+      ])
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('view', [
+        'type' => 'string',
+        'label' => 'inline',
+        'weight' => -10,
+      ]);
+
+    $fields['abbreviation'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Abbreviation'))
+      ->setDescription(t('The abbreviated name of the Research Programme.'))
+      ->setRevisionable(TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'settings' => [
+          'size' => 25,
+        ],
+      ])
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('view', [
+        'type' => 'string',
+        'label' => 'inline',
+      ]);
+
+    $fields['principal_investigators'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Principal Investigators'))
+      ->setRevisionable(TRUE)
+      ->setCardinality(FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED)
+      ->setSetting('target_type', 'rothamsted_researcher')
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'settings' => [
+          'match_operator' => 'CONTAINS',
+          'size' => 60,
+          'placeholder' => '',
+        ],
+      ])
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'inline',
+        'type' => 'entity_reference_label',
+      ]);
+
+    $fields['start'] = BaseFieldDefinition::create('timestamp')
+      ->setLabel(t('Start date'))
+      ->setDescription(t('The start date of the program.'))
+      ->setRevisionable(TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'datetime_timestamp_optional',
+      ])
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('view', [
+        'type' => 'datetime_default',
+        'label' => 'inline',
+        'settings' => [
+          'date_format' => 'html_date',
+          'custom_date_format' => '',
+          'timezone' => '',
+        ],
+      ]);
+
+    $fields['end'] = BaseFieldDefinition::create('timestamp')
+      ->setLabel(t('End date'))
+      ->setDescription(t('The end date of the program.'))
+      ->setRevisionable(TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'datetime_timestamp_optional',
+      ])
+      ->setDisplayConfigurable('view', TRUE)
+      ->setDisplayOptions('view', [
+        'type' => 'datetime_default',
+        'label' => 'inline',
+        'settings' => [
+          'date_format' => 'html_date',
+          'custom_date_format' => '',
+          'timezone' => '',
+        ],
+      ]);
+
+    return $fields;
+  }
+
+}
