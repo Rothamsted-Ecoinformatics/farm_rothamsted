c121426131d1923ba0e06a25078518eac1e9647b
Paul Weidner <paul.weidner@gmail.com>
2023-08-15 08:39:47 -0700

    Add notification setting so researchers can turn off email notifications #485

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Filter research entities by experiment category. [#507](https://github.com/Rothamsted-Ecoinformatics/farm_rothamsted/issues/507)
 - Visualize proposals by status. [#503](https://github.com/Rothamsted-Ecoinformatics/farm_rothamsted/issues/503)
+- Add notification setting so researchers can turn off email notifications. [#485](https://github.com/Rothamsted-Ecoinformatics/farm_rothamsted/issues/485)
 
 ### Changed
 
--- modules/farm_rothamsted_researcher/farm_rothamsted_researcher.post_update.php
+++ modules/farm_rothamsted_researcher/farm_rothamsted_researcher.post_update.php
@@ -5,6 +5,7 @@
  * Update hooks for farm_rothamsted.module.
  */
 
+use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\views\Entity\View;
 use Symfony\Component\Yaml\Yaml;
 
@@ -31,4 +32,32 @@ function farm_rothamsted_researcher_post_update_2_14_remove_orcid_prefix(&$sandb
       ->condition('orcid', 'https://orcid.org/%', 'LIKE')
       ->execute();
   }
+}
+
+/**
+ * Add fields to proposal entity.
+ */
+function farm_rothamsted_researcher_post_update_2_16_notification_field(&$sandbox = NULL) {
+
+  // Create notification_enabled field.
+  $field_definition = BaseFieldDefinition::create('boolean')
+    ->setLabel('Notifications')
+    ->setDefaultValue(TRUE)
+    ->setRevisionable(TRUE);
+  \Drupal::entityDefinitionUpdateManager()->installFieldStorageDefinition(
+    'notification_enabled',
+    'rothamsted_researcher',
+    'farm_rothamsted_researcher',
+    $field_definition,
+  );
+
+  // Set default value for existing researchers.
+  \Drupal::database()->update('rothamsted_researcher_data')
+    ->isNull('notification_enabled')
+    ->fields(['notification_enabled' => TRUE])
+    ->execute();
+  \Drupal::database()->update('rothamsted_researcher_field_revision')
+    ->isNull('notification_enabled')
+    ->fields(['notification_enabled' => TRUE])
+    ->execute();
 }
