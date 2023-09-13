810c8ed0a501aaea3cbab9b0b00f4798941bda3c
Paul Weidner <paul.weidner@gmail.com>
2023-09-13 10:12:21 -0700

    Create generalize ResearchPermissionProvider class with bundle logic

--- modules/farm_rothamsted_experiment_research/src/ResearchEntityPermissionProvider.php
+++ modules/farm_rothamsted_experiment_research/src/ResearchEntityPermissionProvider.php
@@ -15,18 +15,8 @@ class ResearchEntityPermissionProvider extends UncacheableEntityPermissionProvid
    */
   public function buildEntityTypePermissions(EntityTypeInterface $entity_type) {
     $permissions = parent::buildEntityTypePermissions($entity_type);
-    $entity_type_id = $entity_type->id();
-    $plural_label = $entity_type->getPluralLabel();
-    $permissions["view research_assigned {$entity_type_id}"] = [
-      'title' => $this->t('View research-assigned @type', [
-        '@type' => $plural_label,
-      ]),
-    ];
-    $permissions["update research_assigned {$entity_type_id}"] = [
-      'title' => $this->t('Update research-assigned @type', [
-        '@type' => $plural_label,
-      ]),
-    ];
+    $research_permission_provider = \Drupal::classResolver(ResearchPermissionProvider::class);
+    $permissions += $research_permission_provider->buildPermissions($entity_type);
     return $permissions;
   }
 
---
+++ modules/farm_rothamsted_experiment_research/src/ResearchPermissionProvider.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Drupal\farm_rothamsted_experiment_research;
+
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\entity\EntityPermissionProviderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides research_assigned permissions for entity types.
+ */
+class ResearchPermissionProvider implements EntityPermissionProviderInterface, ContainerInjectionInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The entity type bundle info.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeBundleInfoInterface
+   */
+  protected $entityTypeBundleInfo;
+
+  /**
+   * Constructs a TaxonomyViewsIntegratorPermissions instance.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
+   *   The entity type bundle info service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_manager, EntityTypeBundleInfoInterface $entity_type_bundle_info) {
+    $this->entityTypeManager = $entity_manager;
+    $this->entityTypeBundleInfo = $entity_type_bundle_info;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('entity_type.bundle.info'),
+    );
+  }
+
+  /**
+   * Build research permissions for an entity type.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type.
+   *
+   * @return array
+   *   Permissions array.
+   */
+  public function buildPermissions(EntityTypeInterface $entity_type): array {
+    $has_bundle = (bool) $entity_type->getBundleEntityType();
+    return $has_bundle ? $this->buildBundlePermissions($entity_type) : $this->buildEntityTypePermissions($entity_type);
+  }
+
+  /**
+   * Build research permissions for a given entity type.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type.
+   *
+   * @return array
+   *   Permissions array.
+   */
+  public function buildEntityTypePermissions(EntityTypeInterface $entity_type): array {
+    $permissions = [];
+
+    $entity_type_id = $entity_type->id();
+    $plural_label = $entity_type->getPluralLabel();
+    $permissions["view research_assigned $entity_type_id"] = [
+      'title' => $this->t('View research-assigned @type', [
+        '@type' => $plural_label,
+      ]),
+    ];
+    $permissions["update research_assigned $entity_type_id"] = [
+      'title' => $this->t('Update research-assigned @type', [
+        '@type' => $plural_label,
+      ]),
+    ];
+    return $permissions;
+  }
+
+  /**
+   * Build research permissions for bundles of a given entity type.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type.
+   *
+   * @return array
+   *   Permissions array.
+   */
+  public function buildBundlePermissions(EntityTypeInterface $entity_type): array {
+    $permissions = [];
+
+    $entity_type_id = $entity_type->id();
+    $plural_label = $entity_type->getPluralLabel();
+    $bundles = $this->entityTypeBundleInfo->getBundleInfo($entity_type_id);
+    foreach ($bundles as $bundle_id => $bundle_info) {
+      $permissions["view research_assigned $bundle_id $entity_type_id"] = [
+        'title' => $this->t(
+          'View research-assigned @bundle @entity_type',
+          [
+            '@bundle' => $bundle_info['label'],
+            '@entity_type' => $plural_label,
+          ],
+        ),
+      ];
+      $permissions["update research_assigned $bundle_id $entity_type_id"] = [
+        'title' => $this->t(
+          'Update research-assigned @bundle @entity_type',
+          [
+            '@bundle' => $bundle_info['label'],
+            '@entity_type' => $plural_label,
+          ],
+        ),
+      ];
+    }
+    return $permissions;
+  }
+
+}
