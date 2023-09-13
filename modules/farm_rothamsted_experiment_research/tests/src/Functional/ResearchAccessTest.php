b92571756615b6e59136ba3708b53ea620d1e593
Paul Weidner <paul.weidner@gmail.com>
2023-09-12 18:27:39 -0700

    Add tests for plot research_assigned access

--- modules/farm_rothamsted_experiment_research/tests/src/Functional/ResearchAccessTest.php
+++ modules/farm_rothamsted_experiment_research/tests/src/Functional/ResearchAccessTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\farm_rothamsted_experiment_research\Functional;
 
+use Drupal\asset\Entity\Asset;
 use Drupal\farm_rothamsted_experiment_research\Entity\RothamstedDesign;
 use Drupal\farm_rothamsted_experiment_research\Entity\RothamstedExperiment;
 use Drupal\farm_rothamsted_experiment_research\Entity\RothamstedProgram;
@@ -267,6 +268,142 @@ class ResearchAccessTest extends FarmBrowserTestBase {
     $this->drupalGet("$plan_path/edit");
     $this->assertSession()->statusCodeEquals(200);
     $this->drupalGet("$plan_path/delete");
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+  /**
+   * Test access logic on plots.
+   */
+  public function testPlotAccess() {
+
+    // Create roles for view/update assigned/any.
+    Role::create([
+      'id' => 'plot_view_assigned',
+      'label' => 'View assigned',
+      'permissions' => [
+        'view research_assigned plot asset',
+      ],
+    ])->save();
+    Role::create([
+      'id' => 'plot_view_any',
+      'label' => 'View any',
+      'permissions' => [
+        'view any plot asset',
+      ],
+    ])->save();
+    Role::create([
+      'id' => 'plot_update_assigned',
+      'label' => 'Update assigned',
+      'permissions' => [
+        'update research_assigned plot asset',
+      ],
+    ])->save();
+    Role::create([
+      'id' => 'plot_update_any',
+      'label' => 'Update any',
+      'permissions' => [
+        'update any plot asset',
+      ],
+    ])->save();
+
+    // Create a plot and add to the plan.
+    $plot = Asset::create([
+      'type' => 'plot',
+      'plot_id' => 1,
+    ]);
+    $plot->save();
+    $this->plan->set('plot', $plot);
+    $this->plan->save();
+
+    // Plot path.
+    $plot_id = $plot->id();
+    $plot_path = "/asset/$plot_id";
+
+    // Test new user has no access.
+    $this->drupalGet($plot_path);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalGet("$plot_path/edit");
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalGet("$plot_path/delete");
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Grant user view any role.
+    $this->user->addRole('plot_view_any');
+    $this->user->save();
+
+    // Test user only has view access.
+    $this->drupalGet($plot_path);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet("$plot_path/edit");
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalGet("$plot_path/delete");
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Remove role.
+    $this->user->removeRole('plot_view_any');
+    $this->user->save();
+
+    // Add user to the experiment.
+    $this->experiment->set('researcher', [$this->researchers[0], $this->researchers[1]]);
+    $this->experiment->save();
+
+    // Test user has no view access.
+    $this->drupalGet($plot_path);
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalGet("$plot_path/edit");
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalGet("$plot_path/delete");
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Grant user the view assigned role.
+    $this->user->addRole('plot_view_assigned');
+    $this->user->save();
+
+    // Test user only has view access.
+    $this->drupalGet($plot_path);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet("$plot_path/edit");
+    $this->assertSession()->statusCodeEquals(403);
+    $this->drupalGet("$plot_path/delete");
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Grant user the update assigned role.
+    $this->user->addRole('plot_update_assigned');
+    $this->user->save();
+
+    // Test user has view + update access.
+    $this->drupalGet($plot_path);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet("$plot_path/edit");
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet("$plot_path/delete");
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Grant user the update any assigned role.
+    $this->user->removeRole('plot_update_assigned');
+    $this->user->addRole('plot_update_any');
+    $this->user->save();
+
+    // Test user has view + update access.
+    $this->drupalGet($plot_path);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet("$plot_path/edit");
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet("$plot_path/delete");
+    $this->assertSession()->statusCodeEquals(403);
+
+    // Grant user the experiment admin role.
+    $this->user->removeRole('plot_view_assigned');
+    $this->user->removeRole('plot_update_any');
+    $this->user->addRole('rothamsted_experiment_admin');
+    $this->user->save();
+
+    // Test experiment admin role has all access.
+    $this->drupalGet($plot_path);
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet("$plot_path/edit");
+    $this->assertSession()->statusCodeEquals(200);
+    $this->drupalGet("$plot_path/delete");
     $this->assertSession()->statusCodeEquals(200);
   }
 
