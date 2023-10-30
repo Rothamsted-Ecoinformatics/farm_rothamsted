262bb17fae07796f9b0b268b3e3b6976fb5c72ff
Paul Weidner <paul.weidner@gmail.com>
2023-10-29 19:03:10 -0700

    Include roles module in tests

--- modules/farm_rothamsted_experiment_research/tests/src/Functional/ResearchAccessTest.php
+++ modules/farm_rothamsted_experiment_research/tests/src/Functional/ResearchAccessTest.php
@@ -77,6 +77,7 @@ class ResearchAccessTest extends FarmBrowserTestBase {
     'farm_rothamsted_experiment',
     'farm_rothamsted_experiment_research',
     'farm_rothamsted_researcher',
+    'farm_rothamsted_roles',
     'farm_ui_views',
   ];
 
