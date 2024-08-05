acd020bab8a9cd5ff2f79820c5014710e4689317
Paul Weidner <paul.weidner@gmail.com>
2024-08-04 23:37:12 -0700

    Add search form to the dashboard

---
+++ modules/farm_rothamsted_dashboard/src/Plugin/Block/RothamstedSearchBlock.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\farm_rothamsted_dashboard\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a rothamsted search block.
+ *
+ * @Block(
+ *   id = "rothamsted_search",
+ *   admin_label = @Translation("Rothamsted Search")
+ * )
+ */
+class RothamstedSearchBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Form builder service.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
+
+  /**
+   * Constructs a RothamstedSearchBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   *   The form builder service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $form_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return $this->formBuilder->getForm('Drupal\farm_rothamsted_dashboard\Form\RothamstedSearchForm');
+  }
+
+}
