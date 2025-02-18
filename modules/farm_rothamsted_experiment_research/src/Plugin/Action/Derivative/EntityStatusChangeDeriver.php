b38263ec4fc06e0d80676cdf6ef9cb1000bf3858
Paul Weidner <paul.weidner@gmail.com>
2025-02-18 14:46:41 -0800

    Add entity status change action, form, derivation and routing #663

---
+++ modules/farm_rothamsted_experiment_research/src/Plugin/Action/Derivative/EntityStatusChangeDeriver.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\farm_rothamsted_experiment_research\Plugin\Action\Derivative;
+
+use Drupal\Core\Action\Plugin\Action\Derivative\EntityActionDeriverBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+
+/**
+ * Provides an action deriver that finds entity types with a entity status form.
+ */
+class EntityStatusChangeDeriver extends EntityActionDeriverBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDerivativeDefinitions($base_plugin_definition) {
+    if (empty($this->derivatives)) {
+      $definitions = [];
+      foreach ($this->getApplicableEntityTypes() as $entity_type_id => $entity_type) {
+        $definition = $base_plugin_definition;
+        $definition['type'] = $entity_type_id;
+        $definition['label'] = $this->t('Change status');
+        $definition['confirm_form_route_name'] = 'entity.' . $entity_type->id() . '.entity_status_action_form';
+        $definitions[$entity_type_id] = $definition;
+      }
+      $this->derivatives = $definitions;
+    }
+
+    return $this->derivatives;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function isApplicable(EntityTypeInterface $entity_type) {
+    return $entity_type->hasLinkTemplate('entity-status-action-form');
+  }
+
+}
